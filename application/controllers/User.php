b1ebf3d64d250af6c937c5cbb637414afad981c0
novia rahayu <76783234+nobiaa@users.noreply.github.com>
2022-07-28 16:59:44 +0700

    UP Main

---
+++ application/controllers/User.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class User extends CI_Controller
+{
+    public function index()
+    {
+
+        $data['tittle'] = 'My Profile';
+        $data['user'] = $this->db->get_where('user', ['email' =>
+        $this->session->userdata('email')])->row_array();
+
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('user/index', $data);
+        $this->load->view('templates/footer');
+    }
+}
---
+++ application/views/user/index.php
@@ -0,0 +1,14 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+
+    <div class="card mb-3" style="max-width: 540px;">
+        ini halaman user
+    </div>
+
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
